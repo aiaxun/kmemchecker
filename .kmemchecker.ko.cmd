fb1c560ff6853cc1ff773fd048186d08609e8555
axun <unSimple1993@163.com>
2016-07-31 22:49:02 +0800

    source

--- .kmemchecker.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/test/study/memchecker/kmemchecker.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/test/study/memchecker/kmemchecker.ko /home/test/study/memchecker/kmemchecker.o /home/test/study/memchecker/kmemchecker.mod.o
--- .tmp_versions/kmemchecker.mod
+++
@@ -1,2 +0,0 @@
-/home/test/study/memchecker/kmemchecker.ko
-/home/test/study/memchecker/device.o /home/test/study/memchecker/hook.o /home/test/study/memchecker/trackmem.o /home/test/study/memchecker/main.o
